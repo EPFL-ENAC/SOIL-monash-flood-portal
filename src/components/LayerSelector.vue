ab7132a95736d604d9d9524b79bc2e19dfc963ca
ymarcon <yannick.marcon@epfl.ch>
2023-07-03 09:25:53 +0200

    feat: select theme with toggle buttons and maps with select

--- src/components/LayerSelector.vue
+++ src/components/LayerSelector.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { SelectableItem, SelectableSingleItem } from '@/utils/layerSelector'
+import type { SelectableItem, SelectableGroupItem } from '@/utils/layerSelector'
 import { watch, ref, computed } from 'vue'
 
 const props = withDefaults(
@@ -16,7 +16,7 @@ const emit = defineEmits<{
   (e: 'update:modelValue', value: string[]): void
 }>()
 
-const theme = ref<string>()
+const themeIdx = ref<number>()
 const themeItems = computed<any[]>(() =>
   props.items?.filter((item: any) => item.id === 'theme')
     .flatMap((item: any) => item.children)
@@ -28,7 +28,7 @@ const vulnerabilityItems = computed<any[]>(() =>
     .flatMap((item: any) => item.children)
 )
 
-const tabIdx = ref<number>(0)
+const tab = ref<string>()
 const tabItems = computed<any[]>(() =>
   props.items?.filter((item: any) => item.tab)
 )
@@ -37,40 +37,63 @@ const timeIdx = ref<number>(0)
 const timeItems = [20, 50, 100]
 
 const scenarioIdx = ref<number>(0)
-const scenarioItems = ["base", "cc"]
+const scenarioItems = [
+  { 
+    id: "base",
+    label: "Base"
+  }, 
+  { 
+    id: "cc",
+    label: "Climate change"
+  }]
 
 const showInundation = ref<boolean>(false)
 
-watch([theme, tabIdx, showInundation, timeIdx, scenarioIdx, vulnerability], () => {
+watch([themeIdx, tab, showInundation, timeIdx, scenarioIdx, vulnerability], () => {
   updateLayers()
 })
 
 watch(() => props.items,
   (value: SelectableItem[]) => {
-    theme.value = 'light'
-    // vulnerability.value = value.filter((item: SelectableItem) => item.id === 'vulnerability').pop().children[0].id          
+    // init with light theme
+    if (value.filter((item: SelectableItem) => item.id === 'theme').pop())
+      themeIdx.value = 1
+    // init with first map
+    const firstTab = value.filter((item: SelectableItem) => (item as SelectableGroupItem).tab)[0]
+    if (firstTab)
+      tab.value = firstTab.id
+    // init with first vulnerability
+    const vulnerabilityTab = value.filter((item: SelectableItem) => item.id === 'vulnerability').pop()
+    if (vulnerabilityTab)
+      vulnerability.value = (vulnerabilityTab as SelectableGroupItem).children[0].id          
     updateLayers()
   },
   { immediate: true }
 )
 
 function updateLayers() {
-  const type = tabItems.value[tabIdx.value]
-  if (type) {
-    const time = timeItems[timeIdx.value]
-    const scenario = scenarioItems[scenarioIdx.value]
-    const sels = []
-    if (type.id === 'vulnerability')
-      sels.push(vulnerability.value)
-    else
-      sels.push(`${type.id}_${scenario}_${time}`)
-    if (showInundation.value) {
-      sels.push(`depth_${scenario}_${time}`)
-    }
-    sels.push(theme.value)
-    console.log(sels)
-    emit('update:modelValue', sels)
+  const sels = []
+  if (themeIdx.value !== undefined) {
+    sels.push(themeItems.value[themeIdx.value].id)
   }
+  const time = timeItems[timeIdx.value]
+  const scenario = scenarioItems[scenarioIdx.value]
+  const withTimeScenario = time !== undefined && scenario !== undefined
+  if (tab.value) { 
+    const map = tabItems.value.filter((item: SelectableItem) => item.id === tab.value).pop()
+    if (map.id === 'vulnerability') {
+      if (vulnerability.value)
+        sels.push(vulnerability.value)
+    }
+    else if (withTimeScenario) {
+      sels.push(`${map.id}_${scenario.id}_${time}`)
+    }
+  }
+  if (showInundation.value && withTimeScenario) {
+    sels.push(`depth_${scenario.id}_${time}`)
+  }
+  console.log(sels)
+  emit('update:modelValue', sels)
 }
 
 </script>
@@ -78,32 +101,36 @@ function updateLayers() {
 <template>
   <v-card flat>
     <v-card-text class="pa-0">
-      <v-select
-        v-model="theme"
-        label="Theme"
-        :items="themeItems"
-        item-title="label"
-        item-value="id"
-        density="compact"
-      ></v-select>
       <div>
-        <div class="mb-2 text-overline">Type</div>
+        <div class="mb-2 text-overline">Theme</div>
         <v-btn-toggle
-          v-model="tabIdx"
+          v-model="themeIdx"
           divided
           variant="outlined"
         >
-          <v-btn v-for="(item, index) in tabItems" :key="index" size="x-small">{{ item.label }}</v-btn>
+          <v-btn v-for="(item, index) in themeItems" :key="index" size="x-small">{{ item.label }}</v-btn>
         </v-btn-toggle>
+      </div>
+      <div class="mt-2">
+        <div class="mb-2 text-overline">Map</div>
         <v-select
-          v-if="tabIdx === 2"
+          v-model="tab"
+          label="Map"
+          :items="tabItems"
+          item-title="label"
+          item-value="id"
+          density="compact"
+          class="mt-2"
+        ></v-select>
+        <v-select
+          v-if="tab === 'vulnerability'"
           v-model="vulnerability"
           label="Vulnerability"
           :items="vulnerabilityItems"
           item-title="label"
           item-value="id"
           density="compact"
-          class="mt-3"
+          class="mt-2"
         ></v-select>
         <v-checkbox-btn v-model="showInundation" label="Inundation"></v-checkbox-btn>
       </div>
@@ -124,7 +151,7 @@ function updateLayers() {
           divided
           variant="outlined"
         >
-          <v-btn v-for="(item, index) in scenarioItems" :key="index" size="x-small">{{ item }}</v-btn>
+          <v-btn v-for="(item, index) in scenarioItems" :key="index" size="x-small">{{ item.label }}</v-btn>
         </v-btn-toggle>
       </div>
 
